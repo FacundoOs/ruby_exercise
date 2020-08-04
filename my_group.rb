2b6f54c9275a182a525c4396b3fc4098305de4f6
Facundo Osores <facundo@MacBook-Pro-de-Facundo-2.local>
2020-08-04 09:00:34 +0200

    first exercise with ruby

---
+++ my_group.rb
@@ -0,0 +1,4 @@
+my_group[person_1, person_2, person_3]
+person_1 = {name: "Facundo", gender: "masculine", age: 32}
+person_2 = {name: "Mariela", gender: "femenine", age: 32}
+person_3 = {name: "Emiliano", gender: "masculine", age: 30}
