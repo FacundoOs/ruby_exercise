240e3295c4980e25325b471efe6c9d68316cc259
Facundo Osores <facundo@MacBook-Pro-de-Facundo-2.local>
2020-08-06 21:12:16 +0200

    exercises with ruby

--- my_group.rb
+++ my_group.rb
@@ -1,4 +1,12 @@
-my_group[person_1, person_2, person_3]
-person_1 = {name: "Facundo", gender: "masculine", age: 32}
-person_2 = {name: "Mariela", gender: "femenine", age: 32}
-person_3 = {name: "Emiliano", gender: "masculine", age: 30}
+my_group = [person_1[ {name: "Facundo", gender: "masculine", age: 32}],
+            person_2[  {name: "Mariela", gender: "femenine", age: 32}],
+            person_3[  {name: "Emiliano", gender: "masculine", age: 30}]]
+
+
+my_group.each do |algo| # array of array of hashes
+    algo.each do |cosa| # array of hashes
+        cosa.each do |key, value|
+                puts "#{key} => #{value}"
+        end
+    end
+end
